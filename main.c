ca9ccacfe79222ed6fa9846e9ec5f9c790f5290d
aemogie. <theaemogie@gmail.com>
2025-04-01 23:04:31 +0530

    refactor: minor changes

--- main.c
+++ main.c
@@ -5,10 +5,10 @@ int main(void) {
   int flag = 4;
   /* defer(printf("Bye, World!%d\n", flag)); */
   scope(._x = printf("hello\n"), printf("bye\n"), int _x) {
-      printf("Hello, World!\n");
-      printf("%d\n", flag);
+    printf("Hello, World!\n");
+    printf("%d\n", flag);
   }
-  with_resource(file, "main.c", 0) {
+  with_resource(file, __FILE__, 0) {
     char buf[1024];
     read(state.file.fd, buf, sizeof(buf));
     printf("%s", buf);
