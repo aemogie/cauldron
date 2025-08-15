f9c8002b4457fb8d36e713cd4c828a3255d4753a
aemogie <theaemogie@gmail.com>
2025-08-15 15:55:21 +0530

    feat: introduce trace

--- src/sh.h
+++ src/sh.h
@@ -2,5 +2,14 @@
 
 #define echo(...) printf("%s:%d: %s\n", __FILE__, __LINE__, #__VA_ARGS__)
 
+#define SH_TRACE 1
+
+#if SH_TRACE
+#define trace(...) printf("$ %s\n", #__VA_ARGS__)
+#else
+#define trace(...) ((void)0)
+#endif
+
 // todo: error checking
-#define rm(file) (echo(!rm file), unlink(#file))
+#define rm(file) (trace(rm file), unlink(#file))
+
